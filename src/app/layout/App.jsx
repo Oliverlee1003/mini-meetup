6edd589c84d9c9fb0fbe6449f15ddc843875c96f
Oiver Yan Li <oliverlee1003@outlook.com>
2019-08-13 18:47:14 -0700

    basic UI frame with dummy data

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,22 @@
+import React, {Component, Fragment} from 'react';
+import './App.css';
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/navBar/NavBar";
+import {Container} from "semantic-ui-react";
+
+class App extends Component {
+    render(){
+        return (
+            <Fragment>
+                <NavBar/>
+                <Container className= 'main'>
+                    <EventDashboard />
+                </Container>
+            </Fragment>
+
+        );
+    }
+
+}
+
+export default App;
---
+++ src/features/event/EventList/EventList.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import EventListItem from "./EventListItem";
+
+class EventList extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.events.map(event => {
+                    return (<EventListItem key = {event.id} event = {event}/>)
+                })}
+
+            </div>
+        );
+    }
+}
+
+export default EventList;
